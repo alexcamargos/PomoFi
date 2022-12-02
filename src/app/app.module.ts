74e9339f703691cc79a1f376b1a9b261eb721fa6
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-02 07:21:24 -0300

    feat: AppComponent module configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TimerComponent, YoutubePlayerComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, YouTubePlayerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
