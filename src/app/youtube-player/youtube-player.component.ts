06c1bdfa2e6aeb2370d7f17584f661abe526356f
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-02 07:13:27 -0300

    feat: youtube player component

---
+++ src/app/youtube-player/youtube-player.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-youtube-player',
+  templateUrl: './youtube-player.component.html',
+  styleUrls: ['./youtube-player.component.sass'],
+})
+export class YoutubePlayerComponent implements OnInit {
+  apiLoaded: boolean = false;
+
+  ngOnInit() {
+    if (!this.apiLoaded) {
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+      this.apiLoaded = true;
+    }
+  }
+}
