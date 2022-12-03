f0d5ef63f5fd2268ebece3d52ef0c415e3846a1b
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-03 09:49:53 -0300

    feat: implement @angular/cdk/drag-drop in component-container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { YouTubePlayerModule } from '@angular/youtube-player';
 
+import { MaterialModule } from './material.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
 import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, TimerComponent, YoutubePlayerComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, YouTubePlayerModule],
+  declarations: [
+    AppComponent,
+    TimerComponent,
+    YoutubePlayerComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    YouTubePlayerModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
